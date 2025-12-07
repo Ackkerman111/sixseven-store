ba1e8b7bbb95ab3775e852191deb2bc29f48ea35
Ackkerman111 <rotroefx@gmail.com>
2025-12-07 22:47:26 +0530

    Delete js directory

--- js/supabase.js
+++
@@ -1,8 +0,0 @@
-// js/supabase.js
-// Load AFTER the Supabase CDN in each HTML file.
-
-const SUPABASE_URL = "YOUR_SUPABASE_URL";
-const SUPABASE_ANON_KEY = "YOUR_SUPABASE_ANON_KEY";
-
-// This creates the client we use everywhere
-const db = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
